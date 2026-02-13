fc4210bd06871c851e45023eb6ddd1ebaab0c535
skydog221 <skydog221@outlook.com>
2026-02-13 22:11:31 +0800

    chore:作者列表完善

--- src/plugins/todo-list/manifest.ts
+++ src/plugins/todo-list/manifest.ts
@@ -7,5 +7,9 @@ export default {
       name: "多bug的啸天犬 @ CCW",
       link: "https://www.ccw.site/student/197354885",
     },
+    {
+      name: "主核Kernyr (UI Designer)",
+      link: "https://www.hujiarong.site/",
+    },
   ],
 };
